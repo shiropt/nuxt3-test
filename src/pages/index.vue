4dfcbaf10b5f1d06f47ee1f8ddb41638f5f6002a
shiropt <shiro19880610@yahoo.co.jp>
2022-12-18 23:36:20 +0900

    useFetch sample

---
+++ src/pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+type Todo = { userId: number; id: number; title: string; completed: boolean };
+const { data, error } = await useFetch<Todo[]>(() => "https://jsonplaceholder.typicode.com/todos");
+</script>
+<template>
+  <div>
+    <div v-if="error">error</div>
+    <div v-else-if="!data">loading...</div>
+    <div v-else v-for="todo in data" :key="todo.id">
+      <p>{{ todo.title }}</p>
+    </div>
+  </div>
+</template>
